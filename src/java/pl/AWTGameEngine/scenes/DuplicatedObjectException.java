3df7f4e79b3c62e59658765c51ed96831a03144f
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-09-27 16:25:58 +0200

    Delete src/java/pl/AWTGameEngine/scenes/DuplicatedObjectException.java

--- src/java/pl/AWTGameEngine/scenes/DuplicatedObjectException.java
+++
@@ -1,9 +0,0 @@
-package pl.AWTGameEngine.scenes;
-
-public class DuplicatedObjectException extends Exception {
-
-    public DuplicatedObjectException(String identifier) {
-        super("Object with name " + identifier + " already exists in this scene.");
-    }
-
-}
