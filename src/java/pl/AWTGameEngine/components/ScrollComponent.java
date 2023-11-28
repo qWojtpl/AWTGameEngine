0c3d6dbc63743bea6f481412c742c0ae1d46efde
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-11-28 23:19:44 +0100

    Added scroll component serialization methods
    - Added serialization methods and getters and setters to scroll component

--- src/java/pl/AWTGameEngine/components/ScrollComponent.java
+++ src/java/pl/AWTGameEngine/components/ScrollComponent.java
@@ -9,10 +9,12 @@ public class ScrollComponent extends ObjectComponent {
 
     private final BlankRenderer background = new BlankRenderer(getObject());
     private GameObject scroll;
-    private BlankRenderer scrollBackground;
-    private boolean dragged;
+    private BlankRenderer scrollRenderer;
+    private ColorObject scrollColor = new ColorObject("rgb(150,150,150)");
+    private ColorObject selectedScrollColor = new ColorObject("rgb(160,160,160)");
     private int additionalHeight = 0;
-    private boolean highlight = false;
+    private double value = 0;
+    private boolean selected = false;
 
     public ScrollComponent(GameObject object) {
         super(object);
@@ -23,8 +25,8 @@ public class ScrollComponent extends ObjectComponent {
         background.setColor(new ColorObject("rgb(175,175,175)"));
         getObject().addComponent(background);
         scroll = getScene().createGameObject("@vo-" + getObject().getIdentifier() + System.nanoTime());
-        scrollBackground = new BlankRenderer(scroll);
-        scroll.addComponent(scrollBackground);
+        scrollRenderer = new BlankRenderer(scroll);
+        scroll.addComponent(scrollRenderer);
         updatePosition();
     }
 
@@ -42,22 +44,21 @@ public class ScrollComponent extends ObjectComponent {
 
     @Override
     public void onUpdate() {
-        scrollBackground.getColor().setColor("rgb(150,150,150)");
+        scrollRenderer.setColor(scrollColor);
         if((getMouseListener().getMouseX() >= scroll.getX() && getMouseListener().getMouseX() <= scroll.getX() + scroll.getScaleX()
-        && getMouseListener().getMouseY() >= scroll.getY() && getMouseListener().getMouseY() <= scroll.getY() + scroll.getScaleY()) || highlight) {
-            scrollBackground.getColor().setColor("rgb(160,160,160)");
+        && getMouseListener().getMouseY() >= scroll.getY() && getMouseListener().getMouseY() <= scroll.getY() + scroll.getScaleY()) || selected) {
+            scrollRenderer.setColor(selectedScrollColor);
             if(getMouseListener().isMousePressed()) {
-                System.out.println("highlighted");
-                highlight = true;
+                selected = true;
             }
         }
-        updatePosition();
-        if(getMouseListener().isMouseDragged() && highlight) {
+        if(getMouseListener().isMouseDragged() && selected) {
             additionalHeight = getMouseListener().getMouseY() - getObject().getY();
         }
         if(getMouseListener().isMouseReleased()) {
-            highlight = false;
+            selected = false;
         }
+        updatePosition();
     }
 
     private void updatePosition() {
@@ -65,13 +66,60 @@ public class ScrollComponent extends ObjectComponent {
         scroll.setScaleX(getObject().getScaleX());
         scroll.setScaleY(getObject().getScaleY() / 4);
         int y = getObject().getY() + additionalHeight;
+        value = (double) additionalHeight / getObject().getScaleY();
         if(y < getObject().getY()) {
             y = getObject().getY();
+            value = 0;
         }
         if(y > getObject().getY() + getObject().getScaleY() - scroll.getScaleY()) {
             y = getObject().getY() + getObject().getScaleY() - scroll.getScaleY();
+            value = 1;
         }
         scroll.setY(y);
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public boolean isSelected() {
+        return this.selected;
+    }
+
+    public ColorObject getBackgroundColor() {
+        return this.background.getColor();
+    }
+
+    public ColorObject getScrollColor() {
+        return this.scrollColor;
+    }
+
+    public ColorObject getSelectedScrollColor() {
+        return this.selectedScrollColor;
+    }
+
+    public void setBackgroundColor(ColorObject object) {
+        background.setColor(object);
+    }
+
+    public void setBackgroundColor(String color) {
+        background.getColor().setColor(color);
+    }
+
+    public void setScrollColor(ColorObject object) {
+        scrollColor = object;
+    }
+
+    public void setScrollColor(String color) {
+        scrollColor.setColor(color);
+    }
+
+    public void setSelectedScrollColor(ColorObject object) {
+        selectedScrollColor = object;
+    }
+
+    public void setSelectedScrollColor(String color) {
+        selectedScrollColor.setColor(color);
     }
 
 }
