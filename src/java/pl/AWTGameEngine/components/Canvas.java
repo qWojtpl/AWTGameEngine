22c67be1ee8c228c87a977876cf260aaf90d5a74
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-12-02 20:14:07 +0100

    Canvas align
    - Added canvas align for left and right sides of window

--- src/java/pl/AWTGameEngine/components/Canvas.java
+++ src/java/pl/AWTGameEngine/components/Canvas.java
@@ -10,4 +10,34 @@ public class Canvas extends ObjectComponent {
         super(object);
     }
 
+    public void alignObject(CanvasAlign canvasAlign) {
+        if(CanvasAlign.LEFT.equals(canvasAlign)) {
+            getObject().setXForced(0);
+        } else if(CanvasAlign.RIGHT.equals(canvasAlign)) {
+            getObject().setXForced((int) (480 * (getWindow().getPanel().getMultiplier() - 1) - getObject().getScaleX()));
+        }
+    }
+
+    public void setAlign(String align) {
+        try {
+            CanvasAlign canvasAlign = CanvasAlign.valueOf(align.toUpperCase());
+            alignObject(canvasAlign);
+        } catch(IllegalArgumentException e) {
+            alignObject(CanvasAlign.LEFT);
+        }
+    }
+
+    public void alignObject(CanvasAlign[] aligns) {
+        for(CanvasAlign align : aligns) {
+            alignObject(align);
+        }
+    }
+
+    public enum CanvasAlign {
+
+        LEFT,
+        RIGHT
+
+    }
+
 }
