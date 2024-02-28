e7fe9c40ef12f83fb50c3fccb5407623690effaf
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2024-02-28 21:45:13 +0100

    Image Base64
    - Now you can convert sprite into Base64
    - WebPanel image tests

--- src/java/pl/AWTGameEngine/objects/Sprite.java
+++ src/java/pl/AWTGameEngine/objects/Sprite.java
@@ -1,11 +1,23 @@
 package pl.AWTGameEngine.objects;
 
+import pl.AWTGameEngine.engine.Logger;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class Sprite {
 
     private final String imagePath;
     private final Image image;
+    private String base64;
 
     public Sprite(String imagePath, Image image) {
         this.imagePath = imagePath;
@@ -18,6 +30,30 @@ public class Sprite {
 
     public Image getImage() {
         return this.image;
+    }
+
+    public String getImageBase64() {
+        return getImageBase64(false);
+    }
+
+    public String getImageBase64(boolean cacheResult) {
+        if(base64 != null) {
+            return base64;
+        }
+        String returnable;
+        try(ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+            String[] split = imagePath.split("\\.");
+            ImageIO.write((BufferedImage) image, split[split.length - 1], stream);
+            byte[] imageBytes = stream.toByteArray();
+            returnable = new String(Base64.getEncoder().encode(imageBytes), StandardCharsets.UTF_8);
+        } catch(IOException e) {
+            Logger.log("Cannot get Base64 from image " + imagePath, e);
+            return null;
+        }
+        if(cacheResult) {
+            base64 = returnable;
+        }
+        return returnable;
     }
 
 }
