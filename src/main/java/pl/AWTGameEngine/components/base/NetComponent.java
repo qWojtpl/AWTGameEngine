57f68479f056947471940ae248cd3359edcb5960
qWojtpl <wojtoo24@gmail.com>
2026-07-14 00:24:04 +0200

    Docs
    - Added javadocs

--- src/main/java/pl/AWTGameEngine/components/base/NetComponent.java
+++ src/main/java/pl/AWTGameEngine/components/base/NetComponent.java
@@ -11,7 +11,19 @@ import java.net.Socket;
 /**
  * NetComponent is a class which inherits from ObjectComponent
  * and contains events which are common for components which
- * works in the network (eg. onClientConnect, onSynchronize, onNetUpdate).
+ * works in the network (e.g. onClientConnect, onSynchronize, onNetUpdate).
+ * <br><br>
+ * Available events:
+ * <ul>
+ * <li>{@link #onClientConnect(Server, ConnectedClient)}</li>
+ * <li>{@link #onClientDisconnect(Server, ConnectedClient)}</li>
+ * <li>{@link #onClientTryToConnect(Socket)}</li>
+ * <li>{@link #onSynchronize()}</li>
+ * <li>{@link #onSynchronizeReceived(String)}</li>
+ * <li>{@link #onNetUpdate()}</li>
+ * <li>{@link #canSynchronize()}</li>
+ * <li>{@link #clearNetCache()}</li>
+ * </ul>
  */
 public abstract class NetComponent extends ObjectComponent {
 
@@ -33,7 +45,7 @@ public abstract class NetComponent extends ObjectComponent {
      * Event is fired when client tries to connect and after max client check is fired.
      * You can check authentication, blacklist or whitelist here.
      * Return null to let the client in, or return a string, to send a message to the client.
-     * @param socket - Client socket
+     * @param socket Client socket
      * @return null or join-disconnect message
      */
     @EventMethod
@@ -42,7 +54,7 @@ public abstract class NetComponent extends ObjectComponent {
     }
 
     /**
-     * Object synchronize is over a TCP connection to ensure that component will be updated.
+     * Object synchronization is handled over a TCP connection to ensure that component will be updated.
      * onSynchronize event is executed only on server side.
      * @return NetBlock
      */
